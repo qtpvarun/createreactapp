c73210fda6510bb2587ed7f3b067b8da808e58d6
APPLE <apple@APPLEs-MacBook-Pro.local>
2019-10-28 10:34:27 +0530

    Updates

---
+++ precache-manifest.efba4fbe965333dad5cd97c1172f3ed6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94f96691bb2293ecfa9ed02c837b5aa9",
+    "url": "/createreactapp/index.html"
+  },
+  {
+    "revision": "485ea380bcf2ce2bbf83",
+    "url": "/createreactapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "e86fcad29e333ae63bfa",
+    "url": "/createreactapp/static/js/2.b5a413ef.chunk.js"
+  },
+  {
+    "revision": "485ea380bcf2ce2bbf83",
+    "url": "/createreactapp/static/js/main.a4a745c4.chunk.js"
+  },
+  {
+    "revision": "a01ef072653598f4e00b",
+    "url": "/createreactapp/static/js/runtime-main.46dfd299.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/createreactapp/static/media/logo.25bf045c.svg"
+  }
+]);
